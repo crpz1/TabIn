68330f424ee5df644317598d8713875b5efa7f28
crpz1 <8588315+crpz1@users.noreply.github.com>
2022-05-23 21:07:03 +1000

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,73 @@
+﻿using BepInEx;
+using UnboundLib;
+using System;
+using System.Runtime.InteropServices;
+
+namespace TabIn
+{
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin(ModId, ModName, Version)]
+    [BepInProcess("Rounds.exe")]
+    public class Class1 : BaseUnityPlugin
+    {
+        private const string ModId = "faith.mom-gay.TabIn";
+        private const string ModName = "TabIn";
+        public const string Version = "0.0.0";
+        private bool flashing = false;
+        private IntPtr hwnd;
+
+        delegate bool EnumThreadDelegate(IntPtr Hwnd, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern bool EnumThreadWindows(int dwThreadId, EnumThreadDelegate lpfn, IntPtr lParam);
+
+        [DllImport("Kernel32.dll")]
+        static extern int GetCurrentThreadId();
+
+        static IntPtr GetWindowHandle()
+        {
+            IntPtr returnHwnd = IntPtr.Zero;
+            var threadId = GetCurrentThreadId();
+            EnumThreadWindows(threadId,
+                (hWnd, lParam) => {
+                    if (returnHwnd == IntPtr.Zero) returnHwnd = hWnd;
+                    return true;
+                }, IntPtr.Zero);
+            return returnHwnd;
+        }
+
+        private void Start()
+        {
+            Unbound.RegisterClientSideMod(ModId);
+            hwnd = GetWindowHandle();
+        }
+
+        private void Update()
+        {
+            if (UnityEngine.Application.isFocused)
+            {
+                flashing = false;
+                return;
+            }
+            if (GameManager.instance.battleOngoing && GameManager.lockInput == false)
+            {
+                if (!flashing)
+                {
+                    flashing = true;
+                    FlashWindow.Flash(hwnd);
+                }
+            } else if (GameManager.lockInput == false && GameManager.instance.isPlaying)
+            {
+                if (!flashing)
+                {
+                    flashing = true;
+                    FlashWindow.Flash(hwnd);
+                }
+            } else 
+            {
+                flashing = false;
+            }
+            
+        }
+    }
+}
